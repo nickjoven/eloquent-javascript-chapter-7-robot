21ebd3e3c6c3a5596987fad48aaeb1f34381d10a
Nicholas <nicholasjoven@gmail.com>
2022-07-04 10:31:13 -0400

    drew visual of roads array

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert()
+// Goal is to create a delivery robot that picks up and drops off parcels
+// The robot exists in a virtual village initially represented by the roads between
+// each point
+
+const roads = [
+    "Alice's House-Bob's House", "Alice's House-Cabin",
+    "Alice's House-Post Office", "Bob's House-Town Hall",
+    "Daria's House-Ernie's House", "Daria's House-Town Hall",
+    "Ernie's House-Grete's House", "Grete's House-Farm",
+    "Grete's House-Shop", "Marketplace-Farm",
+    "Marketplace-Post Office", "Marketplace-Shop",
+    "Marketplace-Town Hall", "Shop-Town Hall"
+];
+
+// Each element in the array is ordered alphabetically and consist of two parts:
+// "start point-end point" with a "-" character in between
+
+
